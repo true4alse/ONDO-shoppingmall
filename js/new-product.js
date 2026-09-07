40d2e67bb849543e5af9e771e9bd60febc064ec1
정현진 <green4alse@gmail.com>
2026-09-07 11:15:25 +0900

    dddd

---
+++ js/new-product.js
@@ -0,0 +1,11 @@
+let newProductArray = [
+    {
+        pid: 6,
+        pname: '루아 원목 다이닝 테이블',
+        pdesc: `내추럴한 오크 소재와 부드러운 곡선으로 완성한 따뜻한 다이닝 테이블`,
+        price:890000,
+        pdiscount:0,
+        pthumbFileName:'new1.jpg'
+    },
+    
+]
