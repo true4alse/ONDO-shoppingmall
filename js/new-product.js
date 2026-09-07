ebcd710a63e25046451b74bad4c0eeee80033728
정현진 <green4alse@gmail.com>
2026-09-07 12:06:13 +0900

    jhjh

--- js/new-product.js
+++ js/new-product.js
@@ -7,5 +7,93 @@ let newProductArray = [
         pdiscount:0,
         pthumbFileName:'new1.jpg'
     },
+    {
+        pid: 7,
+        pname: '모노 패브릭 소파 3인',
+        pdesc: `담백한 실루엣과 포근한 패브릭이 어우러진 편안한 데일리 소파`,
+        price:1590000,
+        pdiscount:0.2,
+        pthumbFileName:'new2.jpg'
+    },
+    {
+        pid: 8,
+        pname: '오브 사이드 테이블',
+        pdesc: `작은 공간에도 자연스럽게 어우러지는 미니멀한 원형 사이드 테이블`,
+        price:320000,
+        pdiscount:0,
+        pthumbFileName:'new3.jpg'
+    },
+    {
+        pid: 9,
+        pname: '리브 원목 체어',
+        pdesc: `견고한 원목 프레임과 곡선형 등받이로 편안함을 더한 다이닝 체어`,
+        price:390000,
+        pdiscount:0,
+        pthumbFileName:'new4.jpg'
+    },
+    {
+        pid: 10,
+        pname: '아르코 플로어 램프',
+        pdesc: `은은하게 퍼지는 빛과 슬림한 아치형 디자인이 돋보이는 플로어 램프`,
+        price:450000,
+        pdiscount:0,
+        pthumbFileName:'new5.jpg'
+    },
+    {
+        pid: 11,
+        pname: '베르 낮은 수납장',
+        pdesc: `깔끔한 비율과 넉넉한 수납공간으로 거실을 정돈해주는 로우 캐비닛`,
+        price:980000,
+        pdiscount:0,
+        pthumbFileName:'new6.jpg'
+    },
+    {
+        pid: 12,
+        pname: '누아 라운지 체어',
+        pdesc: `깊이감 있는 좌석과 부드러운 패브릭으로 휴식의 순간을 완성하는 라운지 체어`,
+        price:760000,
+        pdiscount:0.15,
+        pthumbFileName:'new7.jpg'
+    },
+    {
+        pid: 13,
+        pname: '오트 침실 협탁',
+        pdesc: `침실에 차분한 분위기를 더해주는 컴팩트한 사이즈의 우드 베드사이드 테이블`,
+        price:290000,
+        pdiscount:0,
+        pthumbFileName:'new8.jpg'
+    },
+    {
+        pid: 14,
+        pname: '셀린 4단 서랍장',
+        pdesc: `군더더기 없는 디자인과 넉넉한 수납으로 공간을 깔끔하게 정리하는 서랍장`,
+        price:1120000,
+        pdiscount:0,
+        pthumbFileName:'new9.jpg'
+    },
+    {
+        pid: 15,
+        pname: '엘로우 벤치 소파',
+        pdesc: `낮고 부드러운 실루엣에 편안한 쿠션감을 더한 모던 벤치형 소파`,
+        price:690000,
+        pdiscount:0.15,
+        pthumbFileName:'new10.jpg'
+    },
+    {
+        pid: 16,
+        pname: '마레 라운드 거울',
+        pdesc: `자연스러운 곡선과 은은한 프레임으로 공간을 한층 넓어 보이게 연출하는 거울`,
+        price:280000,
+        pdiscount:0,
+        pthumbFileName:'new11.jpg'
+    },
+    {
+        pid: 17,
+        pname: '플로우 원목 책상',
+        pdesc: `집중이 필요한 순간을 위한 넓은 상판과 따뜻한 원목 감성의 데스크`,
+        price:740000,
+        pdiscount:0.2,
+        pthumbFileName:'new12.jpg'
+    },
     
 ]
--- js/product-control-new.js
+++ js/product-control-new.js
@@ -1,7 +1,7 @@
 
 
-const saleUlTag = document.querySelector('.sale');
-let result = productArray.map(product => {
+const saleUlTag = document.querySelector('.new-product');
+let result = newProductArray.map(product => {
     return `<li>
                 <a href="#">
                     <figure>
@@ -11,15 +11,17 @@ let result = productArray.map(product => {
                         <h4 class="title-1">${product.pname}</h4>
                         <p class="desc-1">${product.pdesc}</p>
                         <div class="pay-frame">
-                            <div class="pay-original">
+                            ${product.pdiscount?`<div class="pay-original">
                                 <span>${formatMoney(product.price)}</span>원
                             </div>
                             <div class="pay-discount">
                                 <div class="discount">${Math.round(product.pdiscount*100)}%</div>                                
-                                <div class="pay"><b>${formatMoney(product.price * (1-product.pdiscount))}</b>원</div>
-                            </div>
+                                <div class="pay"><b>${formatMoney(product.price *(1-product.pdiscount))}</b>원</div>
+                            </div>`:`<div class="pay-original">
+                                <span>${formatMoney(product.price)}</span>원
+                            </div>`}
+                            
                         </div>
-                        <span class="like-badge noab"><img src="./img/heart-1.svg" alt="좋아요하트">${product.plikes}</span>
                     </div>
                 </a>
             </li>`
